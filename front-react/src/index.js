4acf83dddf6021bc9952117b51b4edce005f70c2
jos4211 <q1w2e3123@naver.com>
2021-02-25 13:17:48 +0900

    서버와 연결 완료 git 메모 작성

--- front-react/src/index.js
+++ front-react/src/index.js
@@ -1,34 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { tempSetUser, check } from './modules/user';
-import createSagaMiddleware from 'redux-saga';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import rootReducer, { rootSaga } from './modules';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { tempSetUser, check } from "./modules/user";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import rootReducer, { rootSaga } from "./modules";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 function loadUser() {
   try {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem("user");
 
     store.dispatch(check());
 
-    if (user === 'null' || user === null) return; // 로그인 상태가 아니라면 아무것도 안함
+    if (user === "null" || user === null) return; // 로그인 상태가 아니라면 아무것도 안함
 
     store.dispatch(tempSetUser(JSON.parse(user)));
     // localStorage.clear();
     store.dispatch(check());
   } catch (e) {
-    console.log('localStorage is not working');
+    console.log("localStorage is not working");
   }
 }
+
+// assignError 잡기위해 실행
+function assignError() {
+  if (typeof Object.assign != "function") {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+      value: function assign(target, varArgs) {
+        // .length of function is 2
+        "use strict";
+        if (target == null) {
+          // TypeError if undefined or null
+          throw new TypeError("Cannot convert undefined or null to object");
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+
+          if (nextSource != null) {
+            // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      },
+      writable: true,
+      configurable: true,
+    });
+  }
+}
+assignError();
 sagaMiddleware.run(rootSaga);
 loadUser();
 ReactDOM.render(
@@ -37,7 +74,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
